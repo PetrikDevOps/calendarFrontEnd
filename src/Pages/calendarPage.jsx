8506c72d99551a808a1f5a3e9e5ff78e5f6caf92
Preisler25 <preisler.andras@gmail.com>
2023-12-05 09:41:48 +0100

    alma

---
+++ src/Pages/calendarPage.jsx
@@ -0,0 +1,12 @@
+import Calendar from "../components/calendar";
+
+const CalendarPage = () => {
+    return (
+        <div>
+            <Calendar />
+        </div>
+    );
+}
+
+export default CalendarPage;
+
---
+++ src/components/calendar.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import api from '../config/axiosConfig';
+
+const Calendar = (id) => {
+    const [msgs, setMsgs] = useState([{day: 1, msg: "Hello", open: false}, {day: 2, msg: "Alma", open: false}]); //[{day: 1, msg: "Hello", open: false}]
+
+    useEffect(() => {
+        api.get(`/api/calendar/?id=${id}`)
+        .then(res => {
+            console.log(res.data);
+            setMsgs(res.data);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }, []);
+
+    const handleClick = (day) => {
+        const newMsgs = msgs.map(msg => {
+            if (msg.day === day) {
+                msg.open = !msg.open;
+            }
+            return msg;
+        });
+        setMsgs(newMsgs);
+    }
+
+    return (
+        <div>
+            <h1>Calendar</h1>
+            <div className="calendar">
+                {msgs.map((msg, index) => (
+                    <div key={index} className="day" onClick={() => handleClick(msg.day)}>
+                        <div className="day-number">{msg.day}</div>
+                        <div className={`day-msg ${msg.open ? "open" : ""}`}>{msg.msg}</div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default Calendar;
