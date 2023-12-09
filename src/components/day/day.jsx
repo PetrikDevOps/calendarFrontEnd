a030af5cff966cd2abce83b6af32534c319305ea
hrustinszkiadam <hrustinszkiadam@gmail.com>
2023-12-09 20:31:38 +0100

    openDay

--- src/components/day/day.jsx
+++ src/components/day/day.jsx
@@ -11,19 +11,8 @@ const Day = (props) => {
 	const [open, setOpen] = useState(true);
 	const [msg, setMsg] = useState('');
 
-	const getDay = () => {
-		api.post('/calendarGet', { id: user.id, day: props.day })
-			.then(({ data }) => {
-				setOpen(data.open);
-				setMsg(data.msg);
-			})
-			.catch((err) => {
-				console.log(err);
-			});
-	};
-
-	const openDay = () => {
-		api.post('/calendarOpen', { id: user.id, day: props.day })
+	const getDay = (openDay = false) => {
+		api.post('/calendar', { id: user.id, day: props.day, openDay })
 			.then(({ data }) => {
 				setOpen(data.open);
 				setMsg(data.msg);
@@ -41,15 +30,11 @@ const Day = (props) => {
 		return (
 			<DayButtonClosed
 				day={props.day}
-				handleClick={openDay}
+				handleClick={getDay(true)}
 			/>
 		);
 	} else {
-		return (
-			<DayButtonOpen
-				msg={msg}
-			/>
-		);
+		return <DayButtonOpen msg={msg} />;
 	}
 };
 
