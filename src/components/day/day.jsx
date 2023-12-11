c0e18d2690b235e6b6dbc5cf2346a6101d4b404d
hrustinszkiadam <hrustinszkiadam@gmail.com>
2023-12-11 23:23:24 +0100

    padding

--- src/components/day/day.jsx
+++ src/components/day/day.jsx
@@ -6,16 +6,18 @@ import api from '../../config/api';
 const Day = (props) => {
 	const { user } = useUser();
 	const [open, setOpen] = useState(false);
-	const [img, setImg] = useState('https://static.vecteezy.com/system/resources/previews/009/398/466/non_2x/present-box-clipart-design-illustration-free-png.png');
+	const [img, setImg] = useState(
+		'https://static.vecteezy.com/system/resources/previews/009/398/466/non_2x/present-box-clipart-design-illustration-free-png.png',
+	);
 	const [msg, setMsg] = useState(props.day);
 
 	const getDay = () => {
 		api.post('/getDay', { id: user.id, day: props.day })
 			.then(({ data }) => {
-                if (data.day == true){
-                    openDay();
+				if (data.day == true) {
+					openDay();
 				}
-                setOpen(data.day);                
+				setOpen(data.day);
 			})
 			.catch((err) => {
 				console.log(err);
@@ -27,7 +29,7 @@ const Day = (props) => {
 		api.post('/openDay', { id: user.id, day: props.day })
 			.then(({ data }) => {
 				setImg(data.msg);
-				openImg()
+				openImg();
 			})
 			.catch((err) => {
 				console.log(err);
@@ -38,8 +40,18 @@ const Day = (props) => {
 		getDay();
 	}, []);
 
-	return <div className='relative self-center justify-self-center bg-red-900 h-15 w-48 text-center text-white text-5xl rounded-lg' onClick={openDay}>{msg}<img src={img} className='absolute left-[80%] top-[20%] h-16'></img></div>;
-
+	return (
+		<div
+			className='h-15 relative w-48 self-center justify-self-center rounded-lg bg-red-900 py-4 text-center text-5xl text-white'
+			onClick={openDay}
+		>
+			{msg}
+			<img
+				src={img}
+				className='absolute left-[80%] top-[20%] h-16'
+			></img>
+		</div>
+	);
 };
 
 export default Day;
